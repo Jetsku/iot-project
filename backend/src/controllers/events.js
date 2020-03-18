dc8e933d263169209fa74bef9ed7915dc06b8905
O3Soft <o3soft0@gmail.com>
2020-03-18 18:57:22 +0200

    Added query strings for event get requests
    /api/events now supports 2 query parameters
    tag returns all entries with that tag
    id returns all entries with id greater than the one provided
    Example: /api/events?id=30&tag=exampletag

--- backend/src/controllers/events.js
+++ backend/src/controllers/events.js
@@ -1,9 +1,17 @@
 const database = require('../database');
+const { Op } = require('sequelize')
 
 exports.list = async (ctx) => {
-  const options = {};
+  const { tag, id } = (ctx.query);
 
-  const events = await database.Event.findAll(options);
+  const tago = tag ? {tag: tag} : {};
+
+  const idint = parseInt(id);
+  const ido = Number.isInteger(idint) ? {id: {[Op.gt]:idint}} : {};
+
+  const options = Object.assign(tago, ido);
+
+  const events = await database.Event.findAll({where: options});
 
   const response = {
     results: events,
@@ -19,7 +27,7 @@ exports.create = async (ctx) => {
   const event = await database.Event.create({
     temperature: params.data.temperature,
     humidity: params.data.humidity,
-    tag: params.tag.id, 
+    tag: params.tag.id,
   });
 
   ctx.body = event;
